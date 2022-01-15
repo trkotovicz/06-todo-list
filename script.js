b0ed450a317c1ccb74d067c15fe91d6a559ecbb8
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-01-15 01:42:52 -0300

    adiciona css e melhora códigos e lint

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-let campoInput = document.getElementById("texto-tarefa");
-let inputButton = document.getElementById("criar-tarefa");
-let taskList = document.getElementById("lista-tarefas");
-
-window.onload = loadTasks;
+const campoInput = document.getElementById('texto-tarefa');
+const inputButton = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 
 // 5 e 6
 function addTarefa() {
-    
-    inputButton.addEventListener("click", function() {
-        if (campoInput.value.length > 0) {
-            let newLi = document.createElement("li");
-            newLi.innerText = campoInput.value;
+  inputButton.addEventListener('click', () => {
+    if (campoInput.value.length > 0) {
+      const newLi = document.createElement('li');
+      newLi.innerText = campoInput.value;
 
-            taskList.appendChild(newLi);
-            campoInput.value = "";
-        }
-    });
-
-    // essa parte não precisava, mas add a função no enter
-    campoInput.addEventListener("keyup", function() {
-        if (event.key === "Enter" && campoInput.value.length > 0) {
-            let newLi = document.createElement("li");
-            newLi.innerText = campoInput.value;
-
-            taskList.appendChild(newLi);
-            campoInput.value = "";
-        }
-    });
-};
+      taskList.appendChild(newLi);
+      campoInput.value = '';
+    }
+  });
+}
 addTarefa();
 
-// 7 e 8
-taskList.addEventListener("click", selected);
+// essa parte não precisava, mas add a função no enter
+function addTarefaEnter() {
+  campoInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter' && campoInput.value.length > 0) {
+      const newLi = document.createElement('li');
+      newLi.innerText = campoInput.value;
+      taskList.appendChild(newLi);
+      campoInput.value = '';
+    }
+  });
+}
+addTarefaEnter();
 
-function selected() {
-    let selected = document.querySelector(".selected");
-    let novaSelecao = event.target;
-    
-    if (selected) {
-        selected.classList.remove("selected");
-    };
-    novaSelecao.classList.add("selected");
-};
+// 7 e 8
+function selected(event) {
+  const selected = document.querySelector('.selected');
+  const novaSelecao = event.target;
+
+  if (selected) {
+    selected.classList.remove('selected');
+  }
+  novaSelecao.classList.add('selected');
+}
+
+taskList.addEventListener('click', selected);
 
 // função anterior que só pinta mas não muda classe e pinta todos itens que clicar
 // function itemCinza() {
@@ -52,73 +51,75 @@ function selected() {
 // itemCinza();
 
 // 9
-taskList.addEventListener("dblclick", completed);
-
 function completed(dblclick) {
-    let completed = dblclick.target;
+  const completed = dblclick.target;
 
-    if (completed.classList.contains("completed")) {
-        completed.classList.remove("completed");
-    } else {
-        completed.classList.add("completed");
-    };
-};
+  if (completed.classList.contains('completed')) {
+    completed.classList.remove('completed');
+  } else {
+    completed.classList.add('completed');
+  }
+}
 
+taskList.addEventListener('dblclick', completed);
 /* // excluí a função pq não entendi como funciona o toggle
 function completed(event) {
     let completed = event.target;
-    completed.classList.toggle("completed"); 
-    // !!!!! NÃO ENTENDI DIREITO COMO FUNCIONA O TOGGLE !!!!!! 
+    completed.classList.toggle("completed");
+    // !!!!! NÃO ENTENDI DIREITO COMO FUNCIONA O TOGGLE !!!!!!
 };
 */
 
 // 10
-let apagaTudo = document.getElementById("apaga-tudo");
-apagaTudo.addEventListener("click", apagaLista);
+const apagaTudo = document.getElementById('apaga-tudo');
 
 function apagaLista() {
-    taskList.innerHTML = "";
-};
+  taskList.innerHTML = '';
+}
+
+apagaTudo.addEventListener('click', apagaLista);
 
 // 11
-let removerFinalizados = document.getElementById("remover-finalizados");
-removerFinalizados.addEventListener("click", apagaCompletos);
+const removerFinalizados = document.getElementById('remover-finalizados');
 
 function apagaCompletos() {
-    let completedTasks = document.querySelectorAll(".completed");
-    
-    // for para percorrer os itens completos, chamar o pai e excluir o filho (da vez na posição index)
-    for (let index = 0; index < completedTasks.length; index += 1) {
-        completedTasks[index].parentElement.removeChild(completedTasks[index]);
-    };
-};
+  const completedTasks = document.querySelectorAll('.completed');
 
-//Bônus
+  // for para percorrer os itens completos, chamar o pai e excluir o filho (da vez na posição index)
+  for (let index = 0; index < completedTasks.length; index += 1) {
+    completedTasks[index].parentElement.removeChild(completedTasks[index]);
+  }
+}
+
+removerFinalizados.addEventListener('click', apagaCompletos);
+
+// Bônus
 // 12
-let salvarTarefas = document.getElementById("salvar-tarefas");
-salvarTarefas.addEventListener("click", saveTasks);
+const salvarTarefas = document.getElementById('salvar-tarefas');
 
 function saveTasks() {
-    localStorage.setItem("tarefas", taskList.innerHTML);
-};
+  localStorage.setItem('tarefas', taskList.innerHTML);
+}
+
+salvarTarefas.addEventListener('click', saveTasks);
 
 // chama a função no onload da pagina
 function loadTasks() {
-    let savedTasks = localStorage.getItem("tarefas");
-    taskList.innerHTML = savedTasks;
-};
+  const savedTasks = localStorage.getItem('tarefas');
+  taskList.innerHTML = savedTasks;
+}
 
-
+window.onload = loadTasks;
 
 // 14
-let removerSelecionado = document.getElementById("remover-selecionado");
-removerSelecionado.addEventListener("click", removeSelected);
+const removerSelecionado = document.getElementById('remover-selecionado');
+removerSelecionado.addEventListener('click', removeSelected);
 
 function removeSelected() {
-    let selected = document.querySelectorAll(".selected");
-    
-    // for para percorrer os itens selecionados, chamar o pai e excluir o filho (no caso, o item selecionado em si)
-    for (let index = 0; index < selected.length; index += 1) {
-        selected[index].parentElement.removeChild(selected[index]);
-    };
-};
+  const selected = document.querySelectorAll('.selected');
+
+  // for para percorrer os itens selecionados, chamar o pai e excluir o filho (no caso, o item selecionado em si)
+  for (let index = 0; index < selected.length; index += 1) {
+    selected[index].parentElement.removeChild(selected[index]);
+  }
+}
